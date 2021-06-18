1e8b5b2cc59040d1ba4a584cfdc9cf6bda8f72f1
GamesTan <gamestan@163.com>
2021-06-18 14:06:06 +0800

    [+] lec01 HelloWorld

---
+++ Lec01_HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec01_HelloWorld {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("HelloWorld");
+            Console.ReadLine();
+        }
+    }
+}
